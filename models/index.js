bbc8810bcb351edcefe6439ca931a3ff7fa5e4ea
Kendra Wing <kendracwing@icloud.com>
2021-05-17 16:31:23 -0600

    Started to link models to models/Index.js File
    Index.js->(Product,Category,Tag,ProductTag js Files) - Files in progress...

--- models/index.js
+++ models/index.js
@@ -5,6 +5,11 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  
+})
+
+
 
 // Categories have many Products
 
